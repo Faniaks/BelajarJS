0226b0d3c11701b685913ca4c8771ca63cf6a715
Faniaks <faniaks554@gmail.com>
2023-10-17 19:18:21 +0700

    Final

--- main.js
+++ main.js
@@ -30,14 +30,64 @@
 
 
 
-let p = document.querySelector("p")
-let button = document.querySelector("button")
-let input = document.querySelector("input")
+// let p = document.querySelector("p")
+// let button = document.querySelector("button")
+// let input = document.querySelector("input")
 
-button.addEventListener('click', function() {
-    let isi = input.value
-    console.log(isi)
-    p.innerHTML = isi
-})
+// button.addEventListener('click', function() {
+//     let isi = input.value
+//     console.log(isi)
+//     p.innerHTML = isi
+// })
+
+function tambahTugas() {
+    const taskText = document.getElementById("task").value;
+
+    if (taskText.trim() === "") {
+        return;
+    }
+
+    const taskList = document.getElementById("daftarTugas");
+
+    const listItem = document.createElement("li");
+    const checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    checkBox.classList.add("checkbox");
+    const taskSpan = document.createElement("span");
+    taskSpan.innerText = taskText;
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "X";
+    deleteButton.classList.add("delete-button");
+    taskSpan.classList.add("task-text");
+
+    // Event listener untuk menandai/mencoret tugas
+    checkBox.addEventListener("change", function () {
+        if (checkBox.checked) {
+            taskSpan.style.textDecoration = "line-through";
+        } else {
+            taskSpan.style.textDecoration = "none";
+        }
+    });
+
+    
+    deleteButton.addEventListener("click", function () {
+        taskList.removeChild(listItem);
+    });
+
+    listItem.appendChild(checkBox);
+    listItem.appendChild(taskSpan);
+    listItem.appendChild(deleteButton);
+    taskList.appendChild(listItem);
+
+    document.getElementById("task").value = "";
+}
+
+
+
+
+
+
+
+  
 
 
